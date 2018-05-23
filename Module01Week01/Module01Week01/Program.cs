7c88f56b90aebeda2bbc2febb0518c95aca3d578
Teodor Stoian <teodoru.stoian@gmail.com>
2018-05-23 14:22:01 +0300

    .

---
+++ Module01Week01/Module01Week01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module01Week01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+            int rngNumber = rng.Next(0, 101);
+            int guess = 0;
+
+            while (guess != rngNumber)
+            {
+                Console.Write("Try to guess my random generated number from 1 to 100:");
+                int.TryParse(Console.ReadLine(), out guess);
+
+                if (guess > rngNumber)
+                {
+                    Console.WriteLine($"Your guess ({guess}) is too big!");
+                }
+                else if (guess < rngNumber)
+                {
+                    Console.WriteLine($"Your guess ({guess}) is too small!");
+                }
+                else
+                {
+                    Console.WriteLine($"Good job! {guess} is the correct number!");
+                }
+
+            }
+        }
+    }
+}
